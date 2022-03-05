3821d26e3313d066053b0dae2f036e3d2c984cd7
ytf123 <1287014821@qq.com>
2022-03-06 04:38:53 +0800

    接口路由

--- src/main.js
+++ src/main.js
@@ -1,25 +1,24 @@
-const Koa = require('koa')
+
 const { APP_PORT } = require('./config/config.default')
 
-const app = new Koa()
+const app = require('./app')
 
-const Router = require('koa-router')
-const indexRouter = new Router()
-indexRouter.get('/', (ctx, next) => {
-    ctx.body = 'hello index'
-})
+// const Router = require('koa-router')
+// const indexRouter = new Router()
+// indexRouter.get('/', (ctx, next) => {
+//     ctx.body = 'hello index'
+// })
 
-const userRouter = new Router()
-userRouter.get('/users', (ctx,next) => {
-    ctx.body = 'hello users'
-})
+// const userRouter = new Router()
+// userRouter.get('/users', (ctx,next) => {
+//     ctx.body = 'hello users'
+// })
 
-app.use(indexRouter.routes())
-app.use(userRouter.routes())
+// app.use(indexRouter.routes())
 
-app.use((ctx, next) => {
-    ctx.body = 'hello api'
-})
+// app.use((ctx, next) => {
+//     ctx.body = 'hello api'
+// })
 
 app.listen(APP_PORT, () => {
     console.log(`serve is running on http://localhost:${ APP_PORT }`)
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -3,3 +3,8 @@ const Router = require('koa-router')
 const router = new Router({prefix: '/users'})
 
 // GET /users/
+router.get('/', (ctx, next) => {
+    ctx.body = 'hello users!'
+})
+
+module.exports = router;
